ee0d017c2058a051e7c056ebce1e5f7805c8154f
WaysterMelo <waystermelo@gmail.com>
2023-01-09 00:27:24 -0300

    front end

---
+++ ecommerce-front/src/user/Signup.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../core/Layout"
+
+const Signup = () => (
+    <Layout title="Signup" description="Signup to Node React E-commerce App"> ... </Layout>
+)
+
+export default Signup
